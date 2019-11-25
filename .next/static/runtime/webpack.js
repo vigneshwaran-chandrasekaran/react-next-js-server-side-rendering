535c446a6747c1645037cdc25a999d7b795bcec4
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-11-25 19:34:16 +0530

    read me file updated with reference links

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0d5c5ba18bd4ef71e6ef";
+/******/ 	var hotCurrentHash = "f422af3ba49673413df5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react-next-js-server-side-rendering
+
+# Reference links
+
+- https://www.freecodecamp.org/news/the-next-js-handbook/ - Freecodecamp
+- https://www.youtube.com/watch?v=IkOVe40Sy0U - Traversy media
